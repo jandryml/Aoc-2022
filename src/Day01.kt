9607c0017fa6c8b0afca31b4685711b26cd08554
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-01 23:35:28 +0100

    Day01 - draft

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var actualValue = 0
+        input.forEach {
+            if (it.isNotEmpty()) {
+                actualValue += it.toInt()
+            } else {
+                if (actualValue > max) {
+                    max = actualValue
+                }
+                actualValue = 0
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
