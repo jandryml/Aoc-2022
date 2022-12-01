5b135a466b6c1802fc6f1c2c9b4d9680dd621b23
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-02 00:13:49 +0100

    Day01 - first solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,29 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var max = 0
-        var actualValue = 0
-        input.forEach {
-            if (it.isNotEmpty()) {
-                actualValue += it.toInt()
-            } else {
-                if (actualValue > max) {
-                    max = actualValue
-                }
-                actualValue = 0
-            }
-        }
-        return max
-    }
+    fun getSumOfHighestElements(input: List<String>, elementCount: Int) =
+        input.joinToString(";")
+            .split(";;")
+            .map { it.split(";").sumOf { calories -> calories.toInt() } }
+            .sortedDescending()
+            .take(elementCount)
+            .sum()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = getSumOfHighestElements(input, 1)
+    fun part2(input: List<String>) = getSumOfHighestElements(input, 3)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     println(part1(testInput))
     println(part2(testInput))
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
