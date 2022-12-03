2d700c57eb4167ca9a05584235bf4fed470029d0
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-03 23:34:29 +0100

    Day01 - change to private func

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,15 @@
+private fun getSumOfHighestElements(input: List<String>, elementCount: Int) =
+    input.joinToString(";")
+        .split(";;")
+        .map { it.split(";").sumOf { calories -> calories.toInt() } }
+        .sortedDescending()
+        .take(elementCount)
+        .sum()
+
+private fun part1(input: List<String>) = getSumOfHighestElements(input, 1)
+private fun part2(input: List<String>) = getSumOfHighestElements(input, 3)
+
 fun main() {
-    fun getSumOfHighestElements(input: List<String>, elementCount: Int) =
-        input.joinToString(";")
-            .split(";;")
-            .map { it.split(";").sumOf { calories -> calories.toInt() } }
-            .sortedDescending()
-            .take(elementCount)
-            .sum()
-
-    fun part1(input: List<String>) = getSumOfHighestElements(input, 1)
-    fun part2(input: List<String>) = getSumOfHighestElements(input, 3)
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     println(part1(testInput))
