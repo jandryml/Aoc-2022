7132af9bbcf227b7b40fae4f2e5604e5f33830e6
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-04 00:45:04 +0100

    Day03 - second solution draft

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,34 @@
 private fun part1(input: List<String>): Int {
-    return input.asSequence().map { it.chunked(it.length / 2).map { it.toCharArray() } }
-        .map { it.reduce {acc, chars -> acc.intersect(chars.asIterable().toSet()).toCharArray() } }
-        .map { it[0] }
-        .map {
-            if (it in 'A'..'Z')
-                return@map it - 'A' + 27
-            if (it in 'a'..'z')
-                return@map it - 'a' + 1
-            throw Exception("Invalid char")
-        }.sum()
+    return findIntersectItemsAndCalculatePriority(
+        input.asSequence()
+            .map { it.chunked(it.length / 2).map { it.toCharArray() } }
+    )
 }
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    return findIntersectItemsAndCalculatePriority(
+        input.chunked(3).asSequence()
+            .map { it.map { it.toCharArray() } }
+    )
+}
+
+private fun findIntersectItemsAndCalculatePriority(sequence: Sequence<List<CharArray>>): Int {
+    return sequence.map { findIntersectItems(it) }
+        .map { it[0] } // we know that only one common item is present
+        .map {
+            return@map mapCharToInt(it)
+        }.sum()
+}
+
+private fun findIntersectItems(it: List<CharArray>) =
+    it.reduce { acc, chars -> acc.intersect(chars.asIterable().toSet()).toCharArray() }
+
+private fun mapCharToInt(character: Char): Int {
+    if (character in 'A'..'Z')
+        return character - 'A' + 27
+    if (character in 'a'..'z')
+        return character - 'a' + 1
+    throw Exception("Invalid char")
 }
 
 fun main() {
@@ -21,7 +37,7 @@ fun main() {
     println(part1(testInput))
     println(part2(testInput))
     check(part1(testInput) == 157)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
