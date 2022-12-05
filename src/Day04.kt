28b6accec19204feef45ea3cc030a7dd5ddbdf17
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-05 23:32:28 +0100

    Day04 - init

---
+++ src/Day04.kt
@@ -0,0 +1,20 @@
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    println(part1(testInput))
+    println(part2(testInput))
+    // check(part1(testInput) == 1)
+    // check(part2(testInput) == 1)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
