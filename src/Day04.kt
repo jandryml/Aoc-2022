668a2f9ba574b824729176d2f6676a69508e312c
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-05 23:43:13 +0100

    Day04 - first solution draft

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,9 @@
+private fun IntRange.isSubrange(other: IntRange) = (this - other).isEmpty()
+
 private fun part1(input: List<String>): Int {
-    return input.size
+    return input.map {
+        it.split(",").map { it.split("-") }.map { (a, b) -> IntRange(a.toInt(), b.toInt()) }
+    }.map { (a, b) -> a.isSubrange(b) || b.isSubrange(a) }.count { it }
 }
 
 private fun part2(input: List<String>): Int {
@@ -11,7 +15,7 @@ fun main() {
     val testInput = readInput("Day04_test")
     println(part1(testInput))
     println(part2(testInput))
-    // check(part1(testInput) == 1)
+    check(part1(testInput) == 2)
     // check(part2(testInput) == 1)
 
     val input = readInput("Day04")
