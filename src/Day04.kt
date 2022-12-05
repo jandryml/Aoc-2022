90c5c24334c1f26ee1ae5795b63953b22c7298e2
Jan Dryml <jan.dryml@etnetera.cz>
2022-12-05 23:49:19 +0100

    Day04 - second solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,13 +1,22 @@
-private fun IntRange.isSubrange(other: IntRange) = (this - other).isEmpty()
+private infix fun IntRange.isSubrange(other: IntRange) = (this - other).isEmpty()
+private infix fun IntRange.isOverlapping(other: IntRange) = this.intersect(other).isNotEmpty()
 
 private fun part1(input: List<String>): Int {
-    return input.map {
-        it.split(",").map { it.split("-") }.map { (a, b) -> IntRange(a.toInt(), b.toInt()) }
-    }.map { (a, b) -> a.isSubrange(b) || b.isSubrange(a) }.count { it }
+    return transferToRanges(input)
+        .map { (a, b) -> a isSubrange b || b isSubrange a }
+        .count { it }
 }
 
 private fun part2(input: List<String>): Int {
-    return input.size
+    return transferToRanges(input)
+        .map { (a, b) -> a isOverlapping b }
+        .count { it }
+}
+
+private fun transferToRanges(input: List<String>) = input.map {
+    it.split(",").map { rawRange ->
+        rawRange.split("-").let { (a, b) -> IntRange(a.toInt(), b.toInt()) }
+    }
 }
 
 fun main() {
@@ -16,7 +25,7 @@ fun main() {
     println(part1(testInput))
     println(part2(testInput))
     check(part1(testInput) == 2)
-    // check(part2(testInput) == 1)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println(part1(input))
